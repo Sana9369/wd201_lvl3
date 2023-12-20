957817585ec6f83a03eb597dde61aea1221fdb53
Sana <145995640+Sana9369@users.noreply.github.com>
2023-12-20 15:38:41 +0000

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,42 +1,37 @@
 const todoList = () => {
-  all = [];
-  const add = (todoItem) => {
-    all.push(todoItem);
-  };
-  const markAsComplete = (index) => {
-    all[index].completed = true;
-  };
-
-  const overdue = () => {
-    return all.filter((todo) => todo.dueDate < today);
-  };
-
-  const dueToday = () => {
-    return all.filter((todo) => todo.dueDate == today);
-  };
-
-  const dueLater = () => {
-    return all.filter((todo) => todo.dueDate > today);
-  };
-
-  const toDisplayableList = (list) => {
-    return list
-      .map(
-        (todo) =>
-          `${todo.completed ? "[x]" : "[ ]"} ${todo.title} ${todo.dueDate == today ? "" : todo.dueDate
-          }`
-      )
-      .join("\n");
-  };
-
-  return {
-    all,
-    add,
-    markAsComplete,
-    overdue,
-    dueToday,
-    dueLater,
-    toDisplayableList,
+all = [];
+const add = (todoItem) => {
+all.push(todoItem);
+};
+const markAsComplete = (index) => {
+all[index].completed = true;
+};
+const overdue = () => {
+return all.filter((todo) => todo.dueDate < today);
+};
+const dueToday = () => {
+return all.filter((todo) => todo.dueDate == today);
+};
+const dueLater = () => {
+return all.filter((todo) => todo.dueDate > today);
+};
+const toDisplayableList = (list) => {
+return list
+.map(
+(todo) =>
+`${todo.completed ? "[x]" : "[ ]"} ${todo.title} ${todo.dueDate == today ? "" : todo.dueDate
+}`
+)
+.join("\n");
+};
+return {
+all,
+add,
+markAsComplete,
+overdue,
+dueToday,
+dueLater,
+toDisplayableList,
   };
 };
 
@@ -61,7 +56,7 @@ todos.add({ title: "Service Vehicle", dueDate: today, completed: false });
 todos.add({ title: "File taxes", dueDate: tomorrow, completed: false });
 todos.add({ title: "Pay electric bill", dueDate: tomorrow, completed: false });
 
-console.log("My Todo-list\n\n");
+console.log("My Todo-list\n");
 
 console.log("Overdue");
 var overdues = todos.overdue();
